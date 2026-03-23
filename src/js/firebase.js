478268f08cdc7795dd1bfe9d92ebbd710b35677e
kisoga4-commits <kisoga4@gmail.com>
2026-03-23 22:39:41 +0700

    Add realtime lobby presence with Firebase RTDB

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 import { firebaseConfig } from "./config/firebase.config.js";
 
@@ -8,3 +9,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const realtimeDb = getDatabase(app);
